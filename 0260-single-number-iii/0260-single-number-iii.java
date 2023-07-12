e848b301953119ef71c0791356e8fd3e2a3f7e72
Saikat sinha <111873014+Saikatsinha007@users.noreply.github.com>
2023-07-11 22:49:25 -0800

    Time: 1 ms (99.95%), Space: 44.7 MB (9.62%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+    int xorResult = 0;
+    for (int num : nums) {
+        xorResult ^= num;
+    }
+    
+    int rightmostBit = xorResult & (-xorResult);
+    int num1 = 0, num2 = 0;
+    for (int num : nums) {
+        if ((num & rightmostBit) != 0) {
+            num1 ^= num;
+        } else {
+            num2 ^= num;
+        }
+    }
+    
+    return new int[]{num1, num2};    
+    }
+}
