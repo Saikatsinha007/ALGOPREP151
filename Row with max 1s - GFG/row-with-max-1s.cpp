77f89657018780943bde7fa718604d2ccd9b3a24
Saikat sinha <111873014+Saikatsinha007@users.noreply.github.com>
2023-12-15 01:17:37 -0800

    Added solution - LeetHub

--- Row with max 1s - GFG/row-with-max-1s.cpp
+++ Row with max 1s - GFG/row-with-max-1s.cpp
@@ -6,30 +6,42 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:
-	int rowWithMax1s(vector<vector<int> > arr, int N, int M) {
+	int rowWithMax1s(vector<vector<int> > mat, int N, int M) {
 	    // code here
-	 int maxOnes = 0;
-    int maxOnesRowIndex = -1;
+// 	 int maxOnes = 0;
+//     int maxOnesRowIndex = -1;
 
-    // Traverse each row
-    for (int i = 0; i < N; ++i) {
-        int onesCount = 0;
+//     // Traverse each row
+//     for (int i = 0; i < N; ++i) {
+//         int onesCount = 0;
 
-        // Count the number of 1's in the current row
-        for (int j = 0; j < M; ++j) {
-            if (arr[i][j] == 1) {
-                onesCount++;
-            }
-        }
+//         // Count the number of 1's in the current row
+//         for (int j = 0; j < M; ++j) {
+//             if (arr[i][j] == 1) {
+//                 onesCount++;
+//             }
+//         }
 
-        // Update maxOnesRowIndex if the current row has more 1's
-        if (onesCount > maxOnes) {
-            maxOnes = onesCount;
-            maxOnesRowIndex = i;
+//         // Update maxOnesRowIndex if the current row has more 1's
+//         if (onesCount > maxOnes) {
+//             maxOnes = onesCount;
+//             maxOnesRowIndex = i;
+//         }
+//     }
+
+//     return maxOnesRowIndex;
+
+ int maxOnesIndex = -1;
+
+    for (int i = 0; i < mat.size(); ++i) {
+        int onesCount = count(mat[i].begin(), mat[i].end(), 1);
+
+        if (onesCount > 0 && (maxOnesIndex == -1 || onesCount > count(mat[maxOnesIndex].begin(), mat[maxOnesIndex].end(), 1))) {
+            maxOnesIndex = i;
         }
     }
 
-    return maxOnesRowIndex;
+    return maxOnesIndex;
 }
 
 };
